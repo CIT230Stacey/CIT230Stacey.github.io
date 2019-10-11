fcd080f3197a77f42c8c92659898f50df33c73f8
CIT230Stacey <atw17007@byui.edu>
2019-10-11 16:10:39 -0600

    Lesson 4

---
+++ lesson4/js/currentdate.js
@@ -0,0 +1,17 @@
+var date = new Date().getFullYear();
+document.getElementById("year").innerHTML = date;
+function loadDate(){
+          var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+          var monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+          var dateObject = new Date();
+          var day = days[dateObject.getDay()];
+          var dateNumber =dateObject.getDate();
+          var month = monthNames[dateObject.getMonth()];  
+          var year = dateObject.getFullYear();
+          
+          document.getElementById("year").innerHTML = year; 
+          document.getElementById("lastModified").innerHTML = day+", "+dateNumber+" "+month+" "+year;
+          }
+        
+      
