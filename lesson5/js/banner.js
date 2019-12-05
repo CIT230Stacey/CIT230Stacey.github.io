8e0abb1004044000d53fbc7b191f7fca35921db5
CIT230Stacey <atw17007@byui.edu>
2019-12-04 20:24:23 -0700

    Lesson 5 fix to submit

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,15 +1,19 @@
+
 var d = new Date();
 
-weekdays = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-var weekday = weekdays[d.getDay()]
+var weekday = weekdays[d.getDay()];
 
-if (weekday == "friday") {
-        document.getElementById("banner").style.visibility = "visible";
-    }
-else{
-    document.getElementById('banner').style.display = 'none';
-	}
-//if (d.getDay() === 5) {
-//    document.getElementById("bannerad").hidden = false;
-//}
+
+
+if
+  (weekday == "Friday"){
+
+var n = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+document.getElementById("banner").innerHTML = n;
+  }
+else
+{
+  document.getElementById("banner").style.display = "none";
+}
